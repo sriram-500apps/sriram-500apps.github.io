e12919fa612aaa60cb2e343634071689695f7e8e
Pothuraju Sri Ram Kumar <84120886+sriram-500apps@users.noreply.github.com>
2021-10-11 13:36:12 +0530

    Add files via upload

--- 0.chunk.botupchat.js
+++ 0.chunk.botupchat.js
@@ -1,9 +1,22 @@
 webpackJsonp([0],{
 
-/***/ 233:
+/***/ 235:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_moments_ago_vue__ = __webpack_require__(245);
+
+/* harmony default export */ __webpack_exports__["default"] = (__WEBPACK_IMPORTED_MODULE_0__components_moments_ago_vue__["a" /* default */]);
+
+/***/ }),
+
+/***/ 240:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(30);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_moment__ = __webpack_require__(115);
 //
 //
 //
@@ -11,102 +24,128 @@ webpackJsonp([0],{
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+
+__WEBPACK_IMPORTED_MODULE_0_vue__["default"].prototype.moment = __WEBPACK_IMPORTED_MODULE_1_moment__["a" /* default */];
 /* harmony default export */ __webpack_exports__["a"] = ({
-  name: "Header",
-  components: {},
-  props: {
-    conversationHeader: Boolean
-  },
-  data: function data() {
+  data() {
     return {
-      UIPrefs: window._Botup.UIPrefs.prefs
+      epochs: ["year", "month", "day", "hour", "minute"],
+      epochs_kr: ["년", "달", "일", "시간", "분"],
+      epochs_jp: ["年", "月", "日", "時", "分"],
+      year: 31536000,
+      month: 2592000,
+      day: 86400,
+      hour: 3600,
+      minute: 60,
+      humanReadable: "",
+      humanDifference: 0,
+      humanWord: "moment"
     };
   },
-  mounted: function mounted() {
-    var _this = this;
 
-    // Hide chat widget when user click on header
-    document.addEventListener("click", function (event) {
-      if (typeof event.target.className == "string" && event.target.className.includes("profile")) return _this.minimize();
-    });
+  props: {
+    prefix: {
+      type: String,
+      default: "posted"
+    },
+    suffix: {
+      type: String,
+      default: "ago"
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    lang: {
+      type: String,
+      default: "en"
+    }
+  },
+
+  mounted() {
+    setInterval(() => {
+      this.getSeconds(this.date);
+    }, 1000);
+  },
+
+  filters: {
+    plural(value, name, lang) {
+      let plural;
+
+      if (value === 0) {
+        if (lang == "kr") {
+          return "몇" + name;
+        } else if (lang == "jp") {
+          return "何" + name;
+        } else {
+          return "a few " + name + "s";
+        }
+      } else if (value > 1) {
+        if (lang == "en") {
+          return value + " " + name + "s";
+        } else {
+          return value + " " + name + "";
+        }
+      } else {
+        return value + " " + name;
+      }
+    }
+
   },
   methods: {
-    minimize: function minimize() {
-      this.$router.push({
-        name: "icon",
-        params: {
-          active: false
-        }
-      }); // To set chat status in vuex while chat end session
+    getSeconds(time) {
+      let seconds = Object(__WEBPACK_IMPORTED_MODULE_1_moment__["a" /* default */])().diff(Object(__WEBPACK_IMPORTED_MODULE_1_moment__["a" /* default */])(time), "seconds");
+      this.humanReadable = this.getDuration(seconds);
 
-      window._Botup.State.commit("setChatStatus", false);
+      if (this.humanReadable) {
+        this.humanDifference = this.humanReadable.interval;
+        this.humanWord = this.humanReadable.humanEpoch;
+      }
     },
-    // Redirect to conversation details view
-    viewHistory: function viewHistory() {
-      if (window._Botup.State.getters.getActiveConversation.length == 0) window._Botup.State.commit("setActiveConversation", window._Botup.State.getters.getCurrentMessages);
-      this.$router.push("/conversations");
-    },
-    // Navigate to active conversation view
-    navigate: function navigate() {
-      var result = window._Botup.State.getters.getActiveConversation; // To set active conversation
 
-      result.forEach(function (obj) {
-        window._Botup.State.commit("setMessages", obj);
-      });
+    getDuration(seconds) {
+      let epoch, interval;
+      let humanEpoch;
 
-      window._Botup.State.commit("setActiveConversation", []);
+      for (let i = 0; i < this.epochs.length; i++) {
+        epoch = this.epochs[i];
 
-      this.$router.push({
-        name: "chat",
-        params: {
-          active: true
+        if (this.lang == "kr") {
+          humanEpoch = this.epochs_kr[i];
+        } else if (this.lang == "jp") {
+          humanEpoch = this.epochs_jp[i];
+        } else {
+          humanEpoch = this.epochs[i];
         }
-      }); // To set chat status in vuex while chat end session
 
-      window._Botup.State.commit("setChatStatus", true);
+        interval = Math.floor(seconds / this[epoch]);
+
+        if (interval >= 1) {
+          return {
+            interval: interval,
+            humanEpoch: humanEpoch
+          };
+        }
+      }
     }
+
   }
 });
 
 /***/ }),
 
-/***/ 239:
+/***/ 245:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"botup-header-profile-section",style:(_vm.UIPrefs.chat_header.css)},[_c('div',{staticClass:"botup-profile-pic-section"},[_c('div',{staticClass:"botup-profile-flex"},[(!_vm.conversationHeader)?_c('b-img',{staticClass:"botup-gravatar",attrs:{"src":_vm.UIPrefs.chat_header.attr.gravatar}}):_c('div',{staticStyle:{"cursor":"pointer"},on:{"click":_vm.navigate}},[_c('b-img',{staticClass:"botup-chevron-left",attrs:{"src":"https://infinity.500apps.com/img/botup/chevron-left.svg"}})],1),_vm._v(" "),_c('div',{staticClass:"botupchat-margin-left",staticStyle:{"cursor":"pointer"},on:{"click":function($event){return _vm.minimize()}}},[(!_vm.conversationHeader)?_c('div',[_c('div',{staticClass:"botup-bot-name"},[_vm._v(_vm._s(_vm.UIPrefs.chat_header.attr.bot_name))]),_vm._v(" "),_c('div',{staticClass:"botup-bot-description"},[_vm._v(_vm._s(_vm.UIPrefs.chat_header.attr.bot_description))])]):_c('div',{staticClass:"botup-bot-conv-name"},[_vm._v("Conversation History")])])],1),_vm._v(" "),(!_vm.conversationHeader)?_c('div',{staticClass:"botup-history-icon-bg"},[_c('button',{staticClass:"botup-widget-history",attrs:{"aria-label":"Open conversation history"},on:{"click":function($event){return _vm.viewHistory()}}},[_c('b-img',{staticClass:"botup-history-icon",attrs:{"src":"https://infinity.500apps.com/img/botup/history-icon.svg"}})],1)]):_vm._e()])])}
-var staticRenderFns = []
-var esExports = { render: render, staticRenderFns: staticRenderFns }
-/* harmony default export */ __webpack_exports__["a"] = (esExports);
-
-/***/ }),
-
-/***/ 61:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Header_vue__ = __webpack_require__(233);
-/* empty harmony namespace reexport */
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_0c3bc31e_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_Header_vue__ = __webpack_require__(239);
-var normalizeComponent = __webpack_require__(29)
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_loader_lib_selector_type_script_index_0_moments_ago_vue__ = __webpack_require__(240);
+/* unused harmony namespace reexport */
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__vue_loader_lib_template_compiler_index_id_data_v_2961c0da_hasScoped_true_buble_transforms_vue_loader_lib_selector_type_template_index_0_moments_ago_vue__ = __webpack_require__(248);
+function injectStyle (ssrContext) {
+  __webpack_require__(246)
+}
+var normalizeComponent = __webpack_require__(22)
 /* script */
 
 
@@ -115,22 +154,62 @@ var normalizeComponent = __webpack_require__(29)
 /* template functional */
 var __vue_template_functional__ = false
 /* styles */
-var __vue_styles__ = null
+var __vue_styles__ = injectStyle
 /* scopeId */
-var __vue_scopeId__ = null
+var __vue_scopeId__ = "data-v-2961c0da"
 /* moduleIdentifier (server only) */
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
-  __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Header_vue__["a" /* default */],
-  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_0c3bc31e_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_Header_vue__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_loader_lib_selector_type_script_index_0_moments_ago_vue__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_1__vue_loader_lib_template_compiler_index_id_data_v_2961c0da_hasScoped_true_buble_transforms_vue_loader_lib_selector_type_template_index_0_moments_ago_vue__["a" /* default */],
   __vue_template_functional__,
   __vue_styles__,
   __vue_scopeId__,
   __vue_module_identifier__
 )
 
-/* harmony default export */ __webpack_exports__["default"] = (Component.exports);
+/* harmony default export */ __webpack_exports__["a"] = (Component.exports);
 
+
+/***/ }),
+
+/***/ 246:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(247);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var update = __webpack_require__(62)("c4ea1b4e", content, true, {});
+
+/***/ }),
+
+/***/ 247:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(61)(false);
+// imports
+
+
+// module
+exports.push([module.i, ".vue-moments-ago[data-v-2961c0da]{font-size:12px}", ""]);
+
+// exports
+
+
+/***/ }),
+
+/***/ 248:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return (_vm.date)?_c('span',{staticClass:"vue-moments-ago"},[_vm._v(_vm._s(_vm.prefix)+" "+_vm._s(_vm._f("plural")(_vm.humanDifference,_vm.humanWord, _vm.lang))+" "+_vm._s(_vm.suffix))]):_vm._e()}
+var staticRenderFns = []
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ __webpack_exports__["a"] = (esExports);
 
 /***/ })
 
--- 2.chunk.botupchat.js
+++ 2.chunk.botupchat.js
@@ -1,151 +1,14 @@
 webpackJsonp([2],{
 
-/***/ 231:
+/***/ 236:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_moments_ago_vue__ = __webpack_require__(245);
-
-/* harmony default export */ __webpack_exports__["default"] = (__WEBPACK_IMPORTED_MODULE_0__components_moments_ago_vue__["a" /* default */]);
-
-/***/ }),
-
-/***/ 238:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(30);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_moment__ = __webpack_require__(113);
-//
-//
-//
-//
-//
-//
-//
-
-
-__WEBPACK_IMPORTED_MODULE_0_vue__["default"].prototype.moment = __WEBPACK_IMPORTED_MODULE_1_moment__["a" /* default */];
-/* harmony default export */ __webpack_exports__["a"] = ({
-  data() {
-    return {
-      epochs: ["year", "month", "day", "hour", "minute"],
-      epochs_kr: ["년", "달", "일", "시간", "분"],
-      epochs_jp: ["年", "月", "日", "時", "分"],
-      year: 31536000,
-      month: 2592000,
-      day: 86400,
-      hour: 3600,
-      minute: 60,
-      humanReadable: "",
-      humanDifference: 0,
-      humanWord: "moment"
-    };
-  },
-
-  props: {
-    prefix: {
-      type: String,
-      default: "posted"
-    },
-    suffix: {
-      type: String,
-      default: "ago"
-    },
-    date: {
-      type: String,
-      required: true
-    },
-    lang: {
-      type: String,
-      default: "en"
-    }
-  },
-
-  mounted() {
-    setInterval(() => {
-      this.getSeconds(this.date);
-    }, 1000);
-  },
-
-  filters: {
-    plural(value, name, lang) {
-      let plural;
-
-      if (value === 0) {
-        if (lang == "kr") {
-          return "몇" + name;
-        } else if (lang == "jp") {
-          return "何" + name;
-        } else {
-          return "a few " + name + "s";
-        }
-      } else if (value > 1) {
-        if (lang == "en") {
-          return value + " " + name + "s";
-        } else {
-          return value + " " + name + "";
-        }
-      } else {
-        return value + " " + name;
-      }
-    }
-
-  },
-  methods: {
-    getSeconds(time) {
-      let seconds = Object(__WEBPACK_IMPORTED_MODULE_1_moment__["a" /* default */])().diff(Object(__WEBPACK_IMPORTED_MODULE_1_moment__["a" /* default */])(time), "seconds");
-      this.humanReadable = this.getDuration(seconds);
-
-      if (this.humanReadable) {
-        this.humanDifference = this.humanReadable.interval;
-        this.humanWord = this.humanReadable.humanEpoch;
-      }
-    },
-
-    getDuration(seconds) {
-      let epoch, interval;
-      let humanEpoch;
-
-      for (let i = 0; i < this.epochs.length; i++) {
-        epoch = this.epochs[i];
-
-        if (this.lang == "kr") {
-          humanEpoch = this.epochs_kr[i];
-        } else if (this.lang == "jp") {
-          humanEpoch = this.epochs_jp[i];
-        } else {
-          humanEpoch = this.epochs[i];
-        }
-
-        interval = Math.floor(seconds / this[epoch]);
-
-        if (interval >= 1) {
-          return {
-            interval: interval,
-            humanEpoch: humanEpoch
-          };
-        }
-      }
-    }
-
-  }
-});
-
-/***/ }),
-
-/***/ 245:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_loader_lib_selector_type_script_index_0_moments_ago_vue__ = __webpack_require__(238);
-/* unused harmony namespace reexport */
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__vue_loader_lib_template_compiler_index_id_data_v_2961c0da_hasScoped_true_buble_transforms_vue_loader_lib_selector_type_template_index_0_moments_ago_vue__ = __webpack_require__(248);
-function injectStyle (ssrContext) {
-  __webpack_require__(246)
-}
-var normalizeComponent = __webpack_require__(29)
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Messages_vue__ = __webpack_require__(238);
+/* empty harmony namespace reexport */
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_0a58169f_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_Messages_vue__ = __webpack_require__(250);
+var normalizeComponent = __webpack_require__(22)
 /* script */
 
 
@@ -154,59 +17,85 @@ var normalizeComponent = __webpack_require__(29)
 /* template functional */
 var __vue_template_functional__ = false
 /* styles */
-var __vue_styles__ = injectStyle
+var __vue_styles__ = null
 /* scopeId */
-var __vue_scopeId__ = "data-v-2961c0da"
+var __vue_scopeId__ = null
 /* moduleIdentifier (server only) */
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
-  __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_loader_lib_selector_type_script_index_0_moments_ago_vue__["a" /* default */],
-  __WEBPACK_IMPORTED_MODULE_1__vue_loader_lib_template_compiler_index_id_data_v_2961c0da_hasScoped_true_buble_transforms_vue_loader_lib_selector_type_template_index_0_moments_ago_vue__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Messages_vue__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_0a58169f_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_Messages_vue__["a" /* default */],
   __vue_template_functional__,
   __vue_styles__,
   __vue_scopeId__,
   __vue_module_identifier__
 )
 
-/* harmony default export */ __webpack_exports__["a"] = (Component.exports);
+/* harmony default export */ __webpack_exports__["default"] = (Component.exports);
 
 
 /***/ }),
 
-/***/ 246:
-/***/ (function(module, exports, __webpack_require__) {
-
-// style-loader: Adds some css to the DOM by adding a <style> tag
-
-// load the styles
-var content = __webpack_require__(247);
-if(typeof content === 'string') content = [[module.i, content, '']];
-if(content.locals) module.exports = content.locals;
-// add the styles to the DOM
-var update = __webpack_require__(60)("c4ea1b4e", content, true, {});
-
-/***/ }),
-
-/***/ 247:
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(59)(false);
-// imports
-
-
-// module
-exports.push([module.i, ".vue-moments-ago[data-v-2961c0da]{font-size:12px}", ""]);
-
-// exports
-
-
-/***/ }),
-
-/***/ 248:
+/***/ 238:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return (_vm.date)?_c('span',{staticClass:"vue-moments-ago"},[_vm._v(_vm._s(_vm.prefix)+" "+_vm._s(_vm._f("plural")(_vm.humanDifference,_vm.humanWord, _vm.lang))+" "+_vm._s(_vm.suffix))]):_vm._e()}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["a"] = ({
+  components: {
+    Message: function Message() {
+      return __webpack_require__.e/* import() */(3).then(__webpack_require__.bind(null, 244));
+    }
+  },
+  data: function data() {
+    return {
+      UIPrefs: window._Botup.UIPrefs.prefs
+    };
+  },
+  mounted: function mounted() {
+    // Automatically scroll down in onload
+    this.scrollToEnd();
+  },
+  computed: {
+    conversations: function conversations() {
+      // Automatically scroll down in onload
+      this.scrollToEnd(); // Get data from mutation state through veux
+
+      return window._Botup.State.getters.getCurrentMessages;
+    }
+  },
+  methods: {
+    // Automatically scroll down while updating conversation
+    scrollToEnd: function scrollToEnd() {
+      setTimeout(function () {
+        var container = document.getElementById("botupchat");
+        if (container) container.scrollTop = container.scrollHeight;
+      }, 0);
+    }
+  }
+});
+
+/***/ }),
+
+/***/ 250:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('div',{staticClass:"body-scroll",style:(_vm.UIPrefs.font_family.css),attrs:{"id":"botupchat"}},[_c('div',{staticClass:"botup-welcome-box"},[_c('div',{staticClass:"botup-welcome-bg"},[_c('div',{staticClass:"botup-welcome-message"},[_vm._v("\n          "+_vm._s(_vm.UIPrefs.chat_header.attr.bot_welcome_msg)+"\n          "),_c('b-img',{attrs:{"src":_vm.UIPrefs.chat_header.attr.welcome_img,"height":"20"}})],1)])]),_vm._v(" "),_vm._l((_vm.conversations),function(list,index){return _c('Message',{key:index,attrs:{"conversations":list}})})],2)])}
 var staticRenderFns = []
 var esExports = { render: render, staticRenderFns: staticRenderFns }
 /* harmony default export */ __webpack_exports__["a"] = (esExports);
