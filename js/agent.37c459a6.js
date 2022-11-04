f07ae701be5528e2f9018ca91de60ecca765b1ad
Pothuraju Sri Ram Kumar <84120886+sriram-500apps@users.noreply.github.com>
2022-11-04 19:08:25 +0530

    Add files via upload

---
+++ js/agent.37c459a6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkably_project"]=self["webpackChunkably_project"]||[]).push([[466],{8634:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var u=function(){var e=this,n=e._self._c;return n("div",[e._v("Agent Page")])},r=[],a={name:"AgentPage",data(){return{}}},l=a,c=t(1001),s=(0,c.Z)(l,u,r,!1,null,null,null),f=s.exports}}]);
+//# sourceMappingURL=agent.37c459a6.js.map
